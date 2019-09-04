a2fb9fd7f4d45d159b369c606052e658f45a6bda
Lukas <lukas.langrock@outlook.de>
2019-09-04 18:41:53 +0200

    Added project files

---
+++ FakeBSOD/BSOD.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace FakeBSOD
+{
+    partial class BSOD
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BSOD));
+            this.BSOD_Text = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BSOD_Text
+            // 
+            this.BSOD_Text.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.BSOD_Text.BackColor = System.Drawing.Color.Transparent;
+            this.BSOD_Text.Font = new System.Drawing.Font("Courier New", 15.75F);
+            this.BSOD_Text.ForeColor = System.Drawing.Color.White;
+            this.BSOD_Text.Location = new System.Drawing.Point(12, 9);
+            this.BSOD_Text.Name = "BSOD_Text";
+            this.BSOD_Text.Size = new System.Drawing.Size(376, 382);
+            this.BSOD_Text.TabIndex = 0;
+            this.BSOD_Text.Text = "BSOD_Text";
+            // 
+            // BSOD
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Blue;
+            this.ClientSize = new System.Drawing.Size(400, 400);
+            this.Controls.Add(this.BSOD_Text);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "BSOD";
+            this.ShowInTaskbar = false;
+            this.Text = "BSOD";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.BSOD_FormClosing);
+            this.Load += new System.EventHandler(this.BSOD_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label BSOD_Text;
+    }
+}
---
+++ FakeBSOD/BSOD.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace FakeBSOD
+{
+    public partial class BSOD : Form
+    {
+        private string BSOD_Content = "A problem has been detected and windows has been shut down to prevent damage to your computer.\n\n" +
+            "DRIVER_IRQL_NOT_LESS_OR_EQUAL\n\n" +
+            "If this is the first time you've seen this stop error screen.\n" +
+            "restart your computer. If this screen appears again, follow these steps:\n\n" +
+            "Check to make sure any new hardware or software is properly installed.\n" +
+            "If this is a new installation, ask your hardware or software manufacturer for any windows updates you might need.\n\n" +
+            "If problems continue, disable or remove any newly installed hardware or software. Disable BIOS memory options such as caching or shadowing.\n" +
+            "If you need to use safe mode to remove or disable components, restart your computer, press F8 to select Advantage Startup Options, and then select Safe Mode.\n\n" +
+            "Technical information:\n\n" +
+            "*** STOP: 0x00000000D1 (0x00000000C, 0x000000002, 0x000000000, 0xF86B5A89)\n\n" +
+            "*** gv3.sys - Address F86B5A89 base at F86B5000, DateStamp 3dd991eb\n\n" +
+            "Beginning dump of physical memory\nPhysical memory dump complete.\n" +
+            "Contact your system administrator or technical support group for further assistance.";
+
+        public BSOD()
+        {
+            InitializeComponent();
+        }
+
+        private void BSOD_Load(object sender, EventArgs e)
+        {
+            Cursor.Hide(); // hide cursor
+            BSOD_Text.Text = BSOD_Content; // put the text on the screen
+        }
+
+        private void BSOD_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!Debugger.IsAttached) { e.Cancel = true; } // makes debugging easier
+        }
+    }
+}
